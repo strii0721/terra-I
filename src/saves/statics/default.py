Fixed joint rotation exceeding +(-)pi in a single trajectory segment.

src/saves/statics/default.py
@@ -55,10 +55,10 @@ HULL = Hull()
 LEFT_ARM = RoboticArm()\
     .construct(Link("l-link_0-0", np.array([5.1, 0, 0])))\
     .construct(Link("l-link_0-1", np.array([0, 5.3, 0])))\
-    .construct(RotationalJoint("la-j1", np.array([0, 1, 0])))\
+    .construct(RotationalJoint("la-j1", np.array([0, 1, 0]), initial_control_variable = 0))\
     .construct(Link("l-link_1-0", np.array([30.891, 0, 0])))\
     .construct(Link("l-link_1-1", np.array([0, -19.45, 0])))\
-    .construct(RotationalJoint("la-j2", np.array([0, -1, 0])))\
+    .construct(RotationalJoint("la-j2", np.array([0, -1, 0]), initial_control_variable = 0))\
     .construct(Link("l-link_2-0", np.array([0, 0, -19.45])))\
     .construct(Link("l-link_2-1", np.array([269.91, 0, 0])))\
     .construct(Link("l-link_2-2", np.array([0, 22.8, 0])))\
@@ -74,7 +74,7 @@ RIGHT_ARM = RoboticArm()\
     .construct(RotationalJoint("ra-j1", np.array([0, -1, 0]), initial_control_variable = pi))\
     .construct(Link("r-link_1-0", np.array([30.891, 0, 0])))\
     .construct(Link("r-link_1-1", np.array([0, -19.45, 0])))\
-    .construct(RotationalJoint("ra-j2", np.array([0, -1, 0])))\
+    .construct(RotationalJoint("ra-j2", np.array([0, -1, 0]), initial_control_variable = 0))\
     .construct(Link("r-link_2-0", np.array([0, 0, -19.45])))\
     .construct(Link("r-link_2-1", np.array([269.91, 0, 0])))\
     .construct(Link("r-link_2-2", np.array([0, 22.8, 0])))\

src/test_inverse_kinematic_analysis.py
@@ -16,49 +16,21 @@
 # Copyright (c) 2025 S.I.C.
 #
 
-from models.robotic_arm import RoboticArm
-from models.link import Link
-from models.rotational_joint import RotationalJoint
-import numpy as np
-from visualization.renderer import Renderer
-from models.hull import Hull
 from math import pi
-from controllers.simulation_controller import SimulationController
 from utils.kinematic_utils import KinematicUtils
 import time
 from dk.logger.log4p import Log4P
+import saves.statics.default as CFG
 
-CONTROL_INTERVAL = 0.1
-RENDER_INTERVAL = 0.1
-
-left_arm = RoboticArm()
-left_arm.construct(Link("link_0-0", np.array([5.1, 0, 0])))\
-    .construct(Link("link_0-1", np.array([0, 5.3, 0])))\
-    .construct(RotationalJoint("la-j1", np.array([0, 1, 0])))\
-    .construct(Link("link_1-0", np.array([30.891, 0, 0])))\
-    .construct(Link("link_1-1", np.array([0, -19.45, 0])))\
-    .construct(RotationalJoint("la-j2", np.array([0, -1, 0])))\
-    .construct(Link("link_2-0", np.array([0, 0, -19.45])))\
-    .construct(Link("link_2-1", np.array([269.91, 0, 0])))\
-    .construct(Link("link_2-2", np.array([0, 22.8, 0])))\
-    .construct(RotationalJoint("la-j3", np.array([0, 1, 0]), initial_control_variable = -pi))\
-    .construct(Link("link_3-0", np.array([250, 0, 0])))
-hull = Hull()
-controller = SimulationController(left_arm).initialize()
-inverse_kinematic_analysis_basis = ["_rf-1",
-                                    "_rf-2",
-                                    "_rf-3",
-                                    "link_3-0"]
-
-controller.bind_inverse_kinematic_analysis_basis(inverse_kinematic_analysis_basis)
-target_control_variable_list = [0, 0, -1*pi/4]
-target_pose_matrix = KinematicUtils.calculate_pose_matrix_dict(left_arm,
-                                                               target_control_variable_list)["link_3-0"]
+target_control_variable_list = [pi/2, 0, pi]
+target_pose_matrix = KinematicUtils.calculate_pose_matrix_dict(CFG.RIGHT_ARM,
+                                                               target_control_variable_list)["r-link_3-0"]
 start = time.time()
-calculated_result = KinematicUtils.inverse_kinematics(left_arm,
+calculated_result = KinematicUtils.inverse_kinematics(CFG.RIGHT_ARM,
                                                       target_pose_matrix,
                                                       learning_rate = 0.2,
-                                                      enable_log = False)
+                                                      shreshold = 1e-6,
+                                                      enable_log = True)
 end = time.time()
 logger = Log4P()
 logger.info(f"Calculation time: {end-start:.6f} sec")

src/utils/kinematic_utils.py
@@ -351,24 +351,29 @@ class KinematicUtils:
             list: An input sequence that can let end of robotic arm reach a given position and orientation.
         """
         logger = Log4P()
-        current_control_variable_list = control_object.retrieve_control_variable_list()
+        currrent_control_variable_list = control_object.retrieve_control_variable_list()
+        target_control_variable_list = currrent_control_variable_list
         for i in range(max_iteration):
             pose_matrixs_dict = KinematicUtils.calculate_pose_matrix_dict(control_object,
-                                                                          current_control_variable_list)
+                                                                          target_control_variable_list)
             current_pose_matrix = list(pose_matrixs_dict.values())[-1]
             po_error = KinematicUtils.calculate_pose_error(target_pose_matrix, current_pose_matrix)
             if enable_log:
-                logger.info(f"[{i}] error norm = {np.linalg.norm(po_error):.6f}, current_control_variables = {current_control_variable_list}")
+                logger.info(f"[{i}] error norm = {np.linalg.norm(po_error):.6f}, current_control_variables = {target_control_variable_list}")
             if np.linalg.norm(po_error) < shreshold:
-                return current_control_variable_list
+                delta_control_variable_list = [
+                    target_control_variable - current_control_variable for target_control_variable, current_control_variable in zip(target_control_variable_list, currrent_control_variable_list)
+                ]
+                delta_control_variable_list = [SpatialUtils.normalize_angle(delta_control_variable) for delta_control_variable in delta_control_variable_list]
+                return [current_control_variable + delta_control_variable for current_control_variable, delta_control_variable in zip(currrent_control_variable_list, delta_control_variable_list)]
             basis_names = control_object.inverse_kinematic_analysis_basis
             basis_pose_matrixs = []
             for name in basis_names:
                 basis_pose_matrixs.append(pose_matrixs_dict[name])
             J = KinematicUtils.calculate_jacobian_matrix(basis_pose_matrixs)
-            delta_control_variables = learning_rate * np.linalg.pinv(J) @ po_error
-            new_control_variables = np.array(current_control_variable_list) + delta_control_variables
-            current_control_variable_list = new_control_variables.tolist()
+            delta_control_variable_vector = learning_rate * np.linalg.pinv(J) @ po_error
+            new_control_variable_vector = np.array(target_control_variable_list) + delta_control_variable_vector
+            target_control_variable_list = new_control_variable_vector.tolist()
         raise RuntimeError("Inverse Kinematic Analysis Failed...")
     
     @staticmethod
@@ -382,9 +387,9 @@ class KinematicUtils:
             return False, str(e)
     
     @staticmethod
-    def average_trajactory_plan(control_object:ComputableAssembly,
-                                target_pose_matrix:np.typing.NDArray,
-                                step_num:int = 50) -> list:
+    def tp_linear_joint_interpolation(control_object:ComputableAssembly,
+                                      target_pose_matrix:np.typing.NDArray,
+                                      step_num:int = 50) -> list:
         is_recachable, result = KinematicUtils.check_reachable(control_object,
                                                                target_pose_matrix)
         if is_recachable:

src/utils/spatial_utils.py
@@ -17,7 +17,7 @@
 #
 
 import numpy as np
-from math import sqrt
+from math import sqrt, pi
 
 class SpatialUtils:
     
@@ -63,3 +63,20 @@ class SpatialUtils:
             sign = np.sign(np.dot(cross_product, normal))
             angle_rad = np.arctan2(np.linalg.norm(cross_product) * sign, dot_product)
             return angle_rad
+    
+    @staticmethod
+    def normalize_angle(angle):
+        """Normalize angle in a range of [-pi, pi]
+
+        Args:
+            angle (float): original angle
+
+        Returns:
+            float: angle after normalization
+        """        
+        while angle < -pi or angle > pi:
+            if angle < -pi:
+                angle += 2*pi
+            if angle > pi:
+                angle -= 2*pi
+        return angle